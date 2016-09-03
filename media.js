1cdf2be4cb897af9d790bff59d4e32b80e770eb1
Jeff Jenkins <jeff@quantastical.com>
2016-09-03 14:38:04 -0400

    Added some color
    Introduced `package.json` configuration file for [Node Package
    Manager](https://www.npmjs.com) and added a dependency for the
    [`colors` package](https://www.npmjs.com/package/colors) to colorize
    the console text.
    
    Introduced the `.gitignore` configuration file for reducing the initial
    project download, omitting the `node_modules` folder that is added when
    the project is installed.

--- media.js
+++ media.js
@@ -1,9 +1,11 @@
+const colors = require("colors")
+
 const appName = "Media"
-const appVersion = "0.0.8"
+const appVersion = "0.0.9"
 
 console.clear = function () {
-	return process.stdout.write('\033c');
+	return process.stdout.write("\033c");
 }
 
 console.clear()
-console.log( appName, `v${appVersion}` )
+console.log( colors.bold.blue.underline(appName, `v${appVersion}`) )
