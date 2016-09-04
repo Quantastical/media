91d6d9059fbe22731a0009c698bc5358608d1f8c
Jeff Jenkins <jeff@quantastical.com>
2016-09-04 13:42:43 -0400

    Implemented Movies Sources
    Added configuration paths for Movies to automatically locate JSON
    metadata in order to populate the Movies menu.

--- media.js
+++ media.js
@@ -1,9 +1,13 @@
-const colors = require("colors")
+const config = require("./config")
 const package = require("./package")
+
+const colors = require("colors")
+const fs = require("fs")
+const glob = require("glob")
+const path = require("path")
 
 const appName = package.name.toUpperCase()
 const appVersion = package.version
-const mediaOptions = [ "Movies" ]
 
 console.clear = function () {
 	return process.stdout.write("\033c")
@@ -25,15 +29,22 @@ console.prompt = function ( message, callback ) {
 	})
 }
 
-var showMenu = function(menuTitle, menuOptions, message) {
+const showMenu = function(menuTitle, menuOptions, message) {
 	console.clear()
 	console.log( menuTitle )
 
-	menuOptions.push("Quit")
+	menuOptions.unshift( {
+		"label": "Home",
+		"action": showMainMenu
+	} )
+	menuOptions.push({
+		"label": "Quit",
+		"action": quit
+	} )
 
 	for( var i = 0; i < menuOptions.length; i++ ) {
 		var optionNumber = i + 1
-		var optionLabel = menuOptions[i]
+		var optionLabel = menuOptions[i].label
 		console.log( "%s. %s", optionNumber, optionLabel )
 	}
 
@@ -47,7 +58,7 @@ var showMenu = function(menuTitle, menuOptions, message) {
 		function(input) {
 			var selectedOption = menuOptions.filter(function(menuOption, index) {
 				if( isNaN(input) ){
-					return menuOption.toLowerCase() == input.toLowerCase()
+					return menuOption.label.toLowerCase() == input.toLowerCase()
 				}
 				else
 					return input == index + 1
@@ -56,16 +67,84 @@ var showMenu = function(menuTitle, menuOptions, message) {
 			if( !selectedOption.length ) {
 				var message = colors.red(colors.bold("Error:"), "You entered an invalid option (" + input.toLowerCase() + ")")
 				return showMenu(menuTitle, menuOptions.filter(function(menuOption) {
-					return menuOption.toLowerCase() != "quit"
+					return menuOption.label.toLowerCase() != "home" && menuOption.label.toLowerCase() != "quit"
 				}), message)
 			}
 
-			console.log( "You selected %s", selectedOption[0] )
-			process.exit()
+			selectedOption[0].action()
 		}
 	)
 }
 
-var mainMenuTitle = colors.bold.blue.underline(appName) + "\n"
-var mainMenuOptions = mediaOptions
-showMenu( mainMenuTitle, mainMenuOptions )
+const showMainMenu = function() {
+	const mainMenuTitle = colors.bold.blue.underline(appName) + "\n"
+	const mainMenuOptions = [
+		{
+			"label": "Movies",
+			"action": showMoviesMenu
+		},
+		{
+			"label": "TV",
+			"action": showTVMenu
+		}
+	]
+	showMenu( mainMenuTitle, mainMenuOptions )
+}
+
+const showMoviesMenu = function() {
+	const moviesMenuTitle = colors.bold.blue.underline(appName + ": Movies") + "\n"
+	const moviesMenuOptions = []
+	const moviesSources = config.sources.filter(function(source){
+		return source.type.toLowerCase() == "movies"
+	})
+
+	var test;
+	moviesSources.forEach(function(moviesSource){
+		var moviesInfos = glob.sync(moviesSource.path + "/**/*.json")
+
+		moviesInfos.forEach(function(movieInfo){
+			var movie = require(movieInfo)
+			
+			var movieMenuOption = {
+				"label": movie.title,
+				"action": function() {
+					showMovie(movie)
+				}
+			}
+			
+			moviesMenuOptions.push(movieMenuOption)
+		})
+	})
+
+	showMenu(moviesMenuTitle, moviesMenuOptions)
+}
+
+const showMovie = function(movie) {
+	const movieMenuTitle = colors.bold.blue.underline(appName + ": " + movie.title + "\n")
+	const movieMenuOptions = [
+		{
+			"label": "Watch Movie",
+			"action": quit
+		}
+	]
+
+	showMenu(movieMenuTitle, movieMenuOptions)
+}
+
+const showTVMenu = function() {
+	const tvMenuTitle = colors.bold.blue.underline(appName + ": TV") + "\n"
+	const tvMenuOptions = [
+		{
+			"label": "Seinfeld",
+			"action": quit
+		}
+	]
+	showMenu(tvMenuTitle, tvMenuOptions)
+}
+
+const quit = function() {
+	console.clear()
+	process.exit()
+}
+
+showMainMenu()
